8d3e8b5d3918347a2e732317f1f7273744530c0c
Adriano Amorim <adryanoamorim@hotmail.com>
2022-11-13 18:15:57 -0300

    implementando tela de relatorio - listagem

--- src/views/RelatoriosPage.vue
+++ src/views/RelatoriosPage.vue
@@ -118,6 +118,96 @@
             </ion-col>
           </ion-row>
         </ion-row>
+
+         <ion-row class="ion-justify-content-start">
+          <ion-col size="12">
+            <ion-text color="secondary">
+              <h5>Listagem de Membros Cadastrados:</h5>
+            </ion-text>
+          </ion-col>
+        </ion-row>
+
+        <ion-row class="ion-justify-content-start borderModalView">
+          <ion-col size="12">
+            <ion-radio-group mode="md">
+              <ion-row class="ion-justify-content-center">
+                <ion-col size="6" class="ion-align-items-center">
+                  <ion-item lines="none" class="diferencaRadioBtn">
+                    <ion-label>CARGO</ion-label>
+                    <ion-radio
+                      slot="start"
+                      value="cargo"
+                      color="secondary"
+                    ></ion-radio>
+                  </ion-item>
+
+                  <ion-item lines="none" class="diferencaRadioBtn">
+                    <ion-label>CARGO e<br />CONGREGAÇÃO</ion-label>
+                    <ion-radio
+                      slot="start"
+                      value="qtdCompleta"
+                      color="secondary"
+                    ></ion-radio>
+                  </ion-item>
+                </ion-col>
+
+                <ion-col size="6" class="ion-align-items-center">
+                  <ion-item lines="none" class="diferencaRadioBtn">
+                    <ion-label>CONGREGAÇÃO</ion-label>
+                    <ion-radio
+                      slot="start"
+                      value="congregacao"
+                      color="secondary"
+                    ></ion-radio>
+                  </ion-item>
+
+                  <ion-item lines="none" class="diferencaRadioBtn">
+                    <ion-label>TODOS</ion-label>
+                    <ion-radio
+                      slot="start"
+                      value="todos"
+                      color="secondary"
+                    ></ion-radio>
+                  </ion-item>
+                </ion-col>
+              </ion-row>
+            </ion-radio-group>
+          </ion-col>
+
+          <ion-col size="12">
+            <ion-row class="ion-justify-content-start">
+              <ion-col size="6">
+                <ion-item mode="md">
+                  <ion-label position="stacked">FILTRO 1 </ion-label>
+                  <ion-select
+                    color="secondary"
+                    placeholder="Selecione"
+                    cancel-text="CANCELAR"
+                    ok-text="OK"
+                  >
+                    <ion-select-option>Missionario</ion-select-option>
+                  </ion-select>
+                </ion-item>
+              </ion-col>
+
+              <ion-col size="6">
+                <ion-item mode="md">
+                  <ion-label position="stacked">FILTRO 2 </ion-label>
+                  <ion-select
+                    color="secondary"
+                    placeholder="Selecione"
+                    cancel-text="CANCELAR"
+                    ok-text="OK"
+                  >
+                    <ion-select-option>Rubem Braga</ion-select-option>
+                  </ion-select>
+                </ion-item>
+              </ion-col>
+            </ion-row>
+          </ion-col>
+
+        </ion-row>
+
       </ion-grid>
     </ion-content>
   </ion-page>
